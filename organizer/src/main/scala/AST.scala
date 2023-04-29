1566e997dd74ac60f0d19f2bedfa56d2c453013e
pc027 <yphoebechen@gmail.com>
2023-04-28 20:06:20 -0700

    Implement parsing for all syntax configurations

--- organizer/src/main/scala/AST.scala
+++ organizer/src/main/scala/AST.scala
@@ -1,5 +1,26 @@
 package docorg.ir
 
 trait Document 
+
 // final object after parsing to create output document
-case class Configurations(path: String, keywords: List[String]) extends Document
+case class Configurations(path: String, keywords: List[KeySegment]) extends Document {
+    override def toString() = "Configurations(" + path + ", " + keywords.map(
+        seg => "(" + seg.toString + ")"
+    ) + ")"
+}
+
+case class KeySegment(words: List[Word]) extends Document {
+    override def toString() = {
+        "<" + words.tail.foldLeft[String](words.head.toString){
+            (a, b) => a + ", " + b
+        } + ">"
+    }
+}
+
+case class Word(str: String, isExact: Boolean = false) extends Document {
+    override def toString() = if isExact then str+"(exact phrasing)" else str
+}
+
+case class OptionalSettings(
+    segLength: Int = -1,
+    outputName: String = "organizer-output.docx") extends Document
--- organizer/src/main/scala/Parser.scala
+++ organizer/src/main/scala/Parser.scala
@@ -4,15 +4,33 @@ import scala.util.parsing.combinator._
 import scala.collection.mutable.ListBuffer
 
 import docorg.ir._
+import java.text.ParseException
 
 // current progress: working on parsers for syntax features in ideal-simple.txt
 object DocumentParser extends JavaTokenParsers with PackratParsers {
   val nonEmptyString = ".+".r
-  // parsing interface
-  def apply(s: String): ParseResult[Configurations] = parseAll(combineSections, s)
+  val integer = "([0-9]+)$".r
 
+  // parsing interface
+  def apply(s: String): ParseResult[(Configurations, OptionalSettings)] = parseAll(combineRequiredOptional, s)
+
+
+  def combineRequiredOptional: Parser[(Configurations, OptionalSettings)] = {
+    combinedSections ~ opt(settings) ^^ {
+      case configs ~ maybeOptions => {
+        maybeOptions match {
+          case Some(optionals) => (configs, optionals)
+          case None => (configs, new OptionalSettings)
+        }
+      }
+    }
+  }
+
+  ////////////////////////////////////////////////////////
+  ////////////// REQUIRED syntax parsing /////////////////
+  ////////////////////////////////////////////////////////
   // combine to parse entire input program
-  def combineSections: Parser[Configurations] = {
+  def combinedSections: Parser[Configurations] = {
     source ~ (("keywords:" ~> wordList)) ^^ {
         case (path) ~ listOfWords => 
             Configurations(path, listOfWords)
@@ -21,10 +39,10 @@ object DocumentParser extends JavaTokenParsers with PackratParsers {
 
   // parses the bulleted list into a list of keywords
   // and then use helper function to create segments
-  def wordList: Parser[List[String]] = {
-    rep("- " ~> nonEmptyString) ^^ {
+  def wordList: Parser[List[KeySegment]] = {
+    rep("- " ~> keyword) ^^ {
       case words => {
-        var keys = ListBuffer.empty[String]
+        var keys = ListBuffer.empty[KeySegment]
         words.foreach(keyword => keys.append(keyword))
 
         keys.to(List)
@@ -32,8 +50,23 @@ object DocumentParser extends JavaTokenParsers with PackratParsers {
     }
   }
   
-  def keyword: Parser[String] =
-    nonEmptyString ^^ {case word => word}
+  def keyword: Parser[KeySegment] =
+    nonEmptyString ^^ {case bulletPoint => {
+      // look for multi-word keys (synonyms) for this segment
+      val words: List[String] = bulletPoint.split(",").toList
+
+      val segments: List[Word] = words.map(w => {
+        val cleanWord = w.trim
+        // check for user-specified exact spelling
+        if (cleanWord.head == '$' && cleanWord.takeRight(1) == "$") {
+          Word(cleanWord.tail.dropRight(1), true)
+        } else {
+          Word(cleanWord)
+        }
+      })
+
+      KeySegment(segments)
+    }}
 
   // parses source to extract file information + format to output
   // "source: essay_brainstorm.txt"
@@ -41,4 +74,39 @@ object DocumentParser extends JavaTokenParsers with PackratParsers {
     "source: " ~> ("[^.]*".r) ~ ("." ~> ("txt"|"docx")) ^^ {
         case filePath ~ format => filePath + "." + format
     }
+
+  ////////////////////////////////////////////////////////
+  ////////////// OPTIONAL syntax parsing /////////////////
+  ////////////////////////////////////////////////////////
+  def settings: Parser[OptionalSettings] =
+    "settings:" ~> rep(option) ^^ {
+      case optConfigs => {
+        var segLength: Int = -1
+        var synonyms: List[String] = List()
+        var outputName: String = "organizer-result.docx"
+
+        optConfigs.foreach((param, value) => {
+          param match
+            case "segLength" => segLength = value.toInt
+            case "outputName" => outputName = value
+            case _ => throw new ParseException(
+              "parsing system error: optional settings parameter must be segLength, synonyms, or outputName", 0)
+        })
+
+        OptionalSettings(segLength, outputName)
+      }
+    }
+  
+  def option: Parser[(String, String)] =
+    // * ~> everything before equals ~> everything after equals (and whitespace, if exists)
+    "* " ~> (("[^=]*".r) ~ (("=" ~> opt(" ")) ~> (nonEmptyString))) ^^ {
+      case optionsKey ~ optionsValue => {
+        optionsKey.trim match
+          case "maximum segment length" => ("segLength", optionsValue)
+          case "output file name" => ("outputName", optionsValue)
+          case _ => throw new ParseException("invalid optional settings found: " +
+            "settings must be 'maximum segment length', 'synonymize', or 'output file name'", 0)
+      }
+    }
+
 }
