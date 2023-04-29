a0af84605c308f036724c975d6844269f5f42ee5
pc027 <yphoebechen@gmail.com>
2023-04-28 22:18:52 -0700

    implement some tests for DSL

--- organizer/src/test/scala/organizer/ParserCheck.scala
+++ organizer/src/test/scala/organizer/ParserCheck.scala
@@ -1,21 +1,98 @@
 package docorg.parser
 
-import org.scalacheck.Prop.forAll
-import org.scalacheck.Properties
 import docorg.ir._
+import scala.io.Source
 
-object DocorgParsePropertySpec extends Properties("Parser") {
+class DocorgParseSpec extends munit.FunSuite {
+  val defaultOptional = OptionalSettings(-1, "organizer-output.docx")
   
-  // some syntactic sugar for expressing parser tests
-  extension (input: String)
-    def ~>(output: Expr) = {
-      val result = CalcParser(input)
-      result.successful && result.get == output
-    }
-  
-  property("minimal required configurations") = {
-    (name: String, keyword: String) => 
-        s"source: $name \n keywords: - $keyword" ~> 
-        Configurations(name, List(KeySegment(List(Word(keyword, false)))))
+  test("minimal required syntax") {
+    val testString = "source: essay-sample-input-synonymize.docx keywords: - hello"
+    val result = DocumentParser(testString)
+    val output = (Configurations("essay-sample-input-synonymize.docx", List(KeySegment(List(Word("hello", false)))))
+      , defaultOptional)
+    assert(result.successful)
+    assertEquals(result.get, output)
+  }
+
+  test("invalid file extension".fail) {
+    val testString = "source: essay-sample-input-synonymize.bad keywords: - hello"
+    val result = DocumentParser(testString)
+    assert(result.successful)
+  }
+
+  test("multiple keywords") {
+    val testString = "source: essay-sample-input-synonymize.docx keywords: - hello \n- hi"
+    val result = DocumentParser(testString)
+    // two resulting segments
+    val first: List[Word] = List(Word("hello", false))
+    val second: List[Word] = List(Word("hi", false))
+    val output = (Configurations("essay-sample-input-synonymize.docx", List(KeySegment(first), KeySegment(second)))
+      , defaultOptional)
+    assert(result.successful)
+    assertEquals(result.get, output)
+  }
+
+  test("multiple keys per keyword") {
+    val testString = "source: essay-sample-input-synonymize.docx keywords: - hello, hi"
+    val result = DocumentParser(testString)
+    // one resulting segment
+    val first: List[Word] = List(Word("hello", false), Word("hi", false))
+    val output = (Configurations("essay-sample-input-synonymize.docx", List(KeySegment(first)))
+      , defaultOptional)
+    assert(result.successful)
+    assertEquals(result.get, output)
+  }
+
+  test("exact keyword") {
+    val testString = "source: essay-sample-input-synonymize.docx keywords: - $hello$"
+    val result = DocumentParser(testString)
+    val exact: List[Word] = List(Word("hello", true))
+    val output = (Configurations("essay-sample-input-synonymize.docx", List(KeySegment(exact)))
+      , defaultOptional)
+    assert(result.successful)
+    assertEquals(result.get, output)
+  }
+
+  test("complex required syntax") {
+    val testString = "source: essay-sample-input-synonymize.docx keywords: - $hello$, hi \n- hola"
+    val result = DocumentParser(testString)
+    // two resulting segments
+    val first: List[Word] = List(Word("hello", true), Word("hi", false))
+    val second: List[Word] = List(Word("hola", false))
+    val output = (Configurations("essay-sample-input-synonymize.docx", List(KeySegment(first), KeySegment(second)))
+      , defaultOptional)
+    assert(result.successful)
+    assertEquals(result.get, output)
+  }
+
+
+  test("invalid keyword delimiter".fail) {
+    val testString = "source: essay-sample-input-synonymize.docx keywords: * $hello$, hi \n* hola"
+    val result = DocumentParser(testString)
+    assert(result.successful)
+  }
+
+  test("valid, but unintended keywords due to spacing") {
+    val testString = "source: essay-sample-input-synonymize.docx keywords: - $hello$, hi - hola"
+    val result = DocumentParser(testString)
+    val first: List[Word] = List(Word("hello", true), Word("hi - hola", false))
+    val output = (Configurations("essay-sample-input-synonymize.docx", List(KeySegment(first)))
+      , defaultOptional)
+    assert(result.successful)
+    assertEquals(result.get, output)
+  }
+
+  test("optional syntax") {
+    val testString = Source.fromFile("../docs/ideal-examples/ideal-fancy.txt").mkString
+    val result = DocumentParser(testString)
+    val first: List[Word] = List(Word("thesis", false), Word("conclusion", false))
+    val second: List[Word] = List(Word("process", true))
+    val third: List[Word] = List(Word("neoliberal", false))
+    val output = (Configurations("sample-input-synonymize.docx"
+        , List(KeySegment(first), KeySegment(second), KeySegment(third)))
+      , OptionalSettings(2, "essay_brainstorm_organized.docx"))
+    assert(result.successful)
+    assertEquals(result.get, output)
   }
 }
--- organizer/src/test/scala/organizer/SystemCheck.scala
+++ organizer/src/test/scala/organizer/SystemCheck.scala
@@ -1,2 +1,33 @@
-// integration tests rather than unit test because of time
+import java.io.{InputStream, FileOutputStream}
+
+import docorg.parser._
+import docorg.ir._
+import docorg.semantics._
+
+// "integration" test rather than unit test because of time
 // but also because the only intermediate function is sortParagraphsIntoSections 
+// and sortParagraphSection requires XWPFParagraph, whose constructor params
+// are difficult to mock
+
+// TODO: test with fixtures: https://scalameta.org/munit/docs/fixtures.html
+class MainInterpreterSpec extends munit.FunSuite {
+
+// tests are commented out because running them will actually generate
+// a file in the root folder
+
+//   test("run ideal-simple") {
+//     runFile("../docs/ideal-examples/ideal-simple.txt")
+//   }
+
+//   test("run ideal-synonym") {
+//     runFile("../docs/ideal-examples/ideal-synonyms.txt")
+//   }
+
+//   test("run ideal-fancy") {
+//     runFile("../docs/ideal-examples/ideal-fancy.txt")
+//   }
+
+  intercept[java.io.FileNotFoundException]{
+    runFile("../docs/ideal-examples/ideal-unfound.txt")
+  }
+}
